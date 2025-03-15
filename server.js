bd80cae109e30c5dd482f6c907f2cd328d607f79
kofzeraa <109040684+kofzeraa@users.noreply.github.com>
2025-03-14 22:53:05 -0300

    Update server.js

--- server.js
+++ server.js
@@ -371,6 +371,6 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
+    console.log(`Server running at https://kofzeraa.github.io/socialbrbank/${port}/`);
 });
 
